9500e6b4efc2de337631a57d7362d33bd14b1b64
Igor <romanow1993@gmail.com>
2018-11-16 15:24:20 +0300

    learn work

---
+++ break.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <conio.h>
+
+int main()
+{
+	const unsigned char WHITE = 219;
+	const unsigned char GRAY = 176; // серый цвет для остальных чисел
+	unsigned char ch;
+	// действия для каждой позиции на экране
+	for (int count = 0; count<80 * 25 - 1; count++)
+	{
+		ch = WHITE; //предполагаем, что число простое
+		for (int j = 2; j<count; j++) // делим на каждое целое, большее 2
+			if (count % j == 0) // если остаток равен 0,
+			{
+				ch = GRAY; // то число не простое
+				break; // выход из внутреннего цикла
+			}
+		cout << ch; // вывод символа на экран
+	}
+	getch(); // задержка полученного изображения
+	return 0;
+}
